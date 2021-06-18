34d560d82e84ca4a6eb006ce29cda1349e86f477
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-19 01:26:41 +0530

    Added:Reverse A number

---
+++ PerfectNumber.java
@@ -0,0 +1,23 @@
+
+package javaLogicalProgram;
+
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        int i, n, Sum = 0 ;
+        System.out.println("\n Please Enter any Number: ");
+        n = Utility.getUserSpecificInteger();
+
+        for(i = 1 ; i < n ; i++) {
+            if(n % i == 0)  {
+                Sum = Sum + i;
+            }
+        }
+        if (Sum == n) {
+            System.out.format("\n% d is a Perfect Number", n);
+        }
+        else {
+            System.out.format("\n% d is NOT a Perfect Number", n);
+        }
+    }
+}
